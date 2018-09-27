9a107c0b375183f2dcbcc703827775fcd92a9cf5
joserm45 <joserodriguez@escolatecnos.cat>
2018-09-27 21:02:07 +0200

    Todo 7

--- Motor2D/j1Scene.cpp
+++ Motor2D/j1Scene.cpp
@@ -67,9 +67,9 @@ bool j1Scene::Update(float dt)
 	// TODO 7: Set the window title like
 	// "Map:%dx%d Tiles:%dx%d Tilesets:%d"
 	p2SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
-					0, 0,
-					0, 0,
-					0);
+					App->map->map.tilewidth, App->map->map.tileheight,
+					App->map->tileset.tilewidth, App->map->tileset.tileheight,
+					App->map->tileset.margin);
 
 	App->win->SetTitle(title.GetString());
 	return true;
